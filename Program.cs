c30edaa1ee6c74a5254b7a0ca66867660e55345e
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-10-15 13:59:58 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿decimal salario, fgts;
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Salário (R$): ");
+salario = Convert.ToDecimal(Console.ReadLine());
+Console.ResetColor();
+
+fgts = (salario / 100) * 8;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"\nFGTS: {fgts:C2}");
+Console.ResetColor();
